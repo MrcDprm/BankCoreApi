40a806e4ee1c89c4f7e6f6f9751841b680b2ba9e
Miraç <mrcdprmm@gmail.com>
2026-07-08 09:48:56 +0300

    feat: veritabanı ve tablolar postgresql üzerinde oluşturuldu.

---
+++ Migrations/20260708064250_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankCoreApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DefterKayitlar",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    HesapId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IslemGrupId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Aciklama = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DefterKayitlar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hesaplar",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    HesapNo = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    HesapSahibiAd = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hesaplar", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hesaplar_HesapNo",
+                table: "Hesaplar",
+                column: "HesapNo",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DefterKayitlar");
+
+            migrationBuilder.DropTable(
+                name: "Hesaplar");
+        }
+    }
+}
